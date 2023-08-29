cc82fd2e649b53dd4fd8d7ddae3407bdb06c2d73
Carlos R <crono_zs@hotmail.com>
2023-08-29 14:47:59 -0600

    agregando cambios de charly

---
+++ VoidRider/Assets/Scripts/Joystic_Char.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystic_Char : MonoBehaviour
+{
+    public Transform parentObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 parentSize = parentObject.localScale;
+        Vector3 relativePosition = parentObject.InverseTransformPoint(transform.position);
+
+        float clampedX = Mathf.Clamp(relativePosition.x, -parentSize.x, parentSize.x);
+        float clampedY = Mathf.Clamp(relativePosition.y, -parentSize.y, parentSize.y);
+        float clampedZ = Mathf.Clamp(relativePosition.z, -parentSize.z, parentSize.z);
+
+        Vector3 clampedPosition = parentObject.TransformPoint(new Vector3(clampedX, clampedY, clampedZ));
+        transform.position = clampedPosition;
+
+    }
+}
