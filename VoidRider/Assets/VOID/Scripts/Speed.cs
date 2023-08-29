f800dd1bc80a5c6f9d995fa9b5a594923a71acad
KaryVelle <kary.velle@gmail.com>
2023-08-29 15:18:11 -0600

    Movimiento

--- VoidRider/Assets/VOID/Scripts/Speed.cs
+++ VoidRider/Assets/VOID/Scripts/Speed.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Speed : MonoBehaviour
 {
-[SerializeField] private Slider mySlider;
-
-
-[SerializeField] private Rigidbody cabin_Rigidbody;
-[SerializeField] private int maxSpeed;
+    [SerializeField] private Slider mySlider;
+    [SerializeField] private Rigidbody cabin_Rigidbody;
+    [SerializeField] private int maxSpeed;
+    [SerializeField] private Vector3 RelativeFwd;
+    [SerializeField] private float speed;
 
 // Start is called before the first frame update
     void Start()
@@ -17,12 +16,22 @@ public class Speed : MonoBehaviour
         mySlider = GetComponent<Slider>();
         if (mySlider == null) Debug.Log("No Slider");
     }
-    
+
+    private void Update()
+    {
+        RelativeFwd = cabin_Rigidbody.transform.TransformDirection(Vector3.forward);
+        Debug.Log(RelativeFwd);
+        cabin_Rigidbody.velocity = RelativeFwd * speed * maxSpeed;
+        Debug.Log(speed);
+    }
+
+
     public void OnValueChanged(float value)
     {
-        Debug.Log(value);
-        float speed = value;
-        cabin_Rigidbody.velocity = cabin_Rigidbody.transform.forward * speed * maxSpeed;
-        
+
+        speed = value;
+        //cabin_Rigidbody.velocity = cabin_Rigidbody.transform.forward * speed * maxSpeed;
+
+
     }
 }
---
+++ VoidRider/Assets/VOID/SliderX.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderX : MonoBehaviour
+{
+    [SerializeField] private Slider mySlider;
+    [SerializeField] private float maxRot;
+    [SerializeField] private Rigidbody cabin_Rigidbody;
+    [SerializeField] private float valueChanged;
+    
+
+// Start is called before the first frame update
+    void Start()
+    {
+        mySlider = GetComponent<Slider>();
+        if (mySlider == null) Debug.Log("No Slider");
+    }
+    
+    public void OnValueChanged(float value)
+    {
+        valueChanged = value * maxRot;
+
+        cabin_Rigidbody.transform.eulerAngles = new Vector3(valueChanged, cabin_Rigidbody.transform.eulerAngles.y,
+            cabin_Rigidbody.transform.eulerAngles.z) ;
+
+    }
+}
