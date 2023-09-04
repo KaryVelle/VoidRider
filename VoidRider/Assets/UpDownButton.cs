3459d9341a782fb219475326a22b98c7ff1d3729
KaryVelle <kary.velle@gmail.com>
2023-09-04 14:16:37 -0600

    MoveButton.notfixed

--- VoidRider/Assets/UpDownButton.cs
+++ VoidRider/Assets/UpDownButton.cs
@@ -4,67 +4,39 @@ using UnityEngine;
 
 public class UpDownButton : MonoBehaviour
 {
-    [SerializeField] private bool upPressed;
-    [SerializeField] private bool downPressed;
+    [SerializeField] private bool upPressed = false;
+   
     [SerializeField] private Rigidbody cabin_Rigidbody;
-    [SerializeField] private float rotDegrees;
+    [SerializeField] private float rotDegrees = 0.5f;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (upPressed)
-        {
-            cabin_Rigidbody.transform.eulerAngles = new Vector3(cabin_Rigidbody.transform.eulerAngles.x + rotDegrees,
-                cabin_Rigidbody.transform.eulerAngles.y, cabin_Rigidbody.transform.eulerAngles.z);
-        }
+       
+          
+        
 
-        if (upPressed!)
-        {
-            rotDegrees = 0;
-
-            cabin_Rigidbody.transform.eulerAngles = new Vector3(cabin_Rigidbody.transform.eulerAngles.x + rotDegrees,
-                cabin_Rigidbody.transform.eulerAngles.y, cabin_Rigidbody.transform.eulerAngles.z);
-
-            if (downPressed)
-            {
-                cabin_Rigidbody.transform.eulerAngles = new Vector3(
-                    cabin_Rigidbody.transform.eulerAngles.x - rotDegrees, cabin_Rigidbody.transform.eulerAngles.y,
-                    cabin_Rigidbody.transform.eulerAngles.z);
-            }
-            if (downPressed!)
-            {
-                cabin_Rigidbody.transform.eulerAngles = new Vector3(cabin_Rigidbody.transform.eulerAngles.x,
-                    cabin_Rigidbody.transform.eulerAngles.y, cabin_Rigidbody.transform.eulerAngles.z);
-                rotDegrees = 0;
-            }
-        }
-
+       
+        
+       
     }
+
+    public void UpPress()
+    {
+        
+        Debug.Log("up");
+    }
+
+    public void UpNotPress()
+    {
+        Debug.Log("notup");
+    }
+
+  
+
+  
     
-    void Up()
-    {
-        upPressed = true;
-
-    }
-
-    void NotUp()
-    {
-        upPressed = false;
-
-    }
-
-    void Down()
-    {
-        upPressed = true;
-
-    }
-
-    void NotDown()
-    {
-        upPressed = false;
-
-    }
 }
    
 
