e23c17db87b913a9a4c6846e63a4306c40c04b02
KaryVelle <kary.velle@gmail.com>
2023-09-04 16:03:57 -0600

    MoveButtons.fixed

--- VoidRider/Assets/UpDownButton.cs
+++ VoidRider/Assets/UpDownButton.cs
@@ -4,36 +4,114 @@ using UnityEngine;
 
 public class UpDownButton : MonoBehaviour
 {
-    [SerializeField] private bool upPressed = false;
+    [SerializeField] public bool upPressed = false;
+    [SerializeField] public bool downPressed = false;
+    [SerializeField] public bool rightPressed = false;
+    [SerializeField] public bool leftPressed = false;
    
-    [SerializeField] private Rigidbody cabin_Rigidbody;
-    [SerializeField] private float rotDegrees = 0.5f;
+    [SerializeField] public Rigidbody cabin_Rigidbody;
+    [SerializeField] public float rotDegrees = 0.5f;
+
+    [SerializeField] public float newRot;
 
 
     // Update is called once per frame
     void Update()
     {
        
-          
-        
-
        
-        
-       
-    }
+            if (upPressed)
+            {
+               
+                cabin_Rigidbody.transform.eulerAngles = new Vector3(
+                    cabin_Rigidbody.transform.eulerAngles.x - rotDegrees,
+                    cabin_Rigidbody.transform.eulerAngles.y ,
+                    cabin_Rigidbody.transform.eulerAngles.z) ;
+            }
+            
+            if (downPressed)
+            {
+               
+                cabin_Rigidbody.transform.eulerAngles = new Vector3(
+                    cabin_Rigidbody.transform.eulerAngles.x + rotDegrees,
+                    cabin_Rigidbody.transform.eulerAngles.y ,
+                    cabin_Rigidbody.transform.eulerAngles.z) ;
+            }
+            
+            if (rightPressed)
+            {
+               
+                cabin_Rigidbody.transform.eulerAngles = new Vector3(
+                    cabin_Rigidbody.transform.eulerAngles.x ,
+                    cabin_Rigidbody.transform.eulerAngles.y + rotDegrees ,
+                    cabin_Rigidbody.transform.eulerAngles.z) ;
+            }
+            
+            if (leftPressed)
+            {
+               
+                cabin_Rigidbody.transform.eulerAngles = new Vector3(
+                    cabin_Rigidbody.transform.eulerAngles.x ,
+                    cabin_Rigidbody.transform.eulerAngles.y - rotDegrees ,
+                    cabin_Rigidbody.transform.eulerAngles.z) ;
+            }
 
-    public void UpPress()
-    {
-        
-        Debug.Log("up");
+
     }
 
     public void UpNotPress()
     {
-        Debug.Log("notup");
+        
+        upPressed = false;
+
+    }
+    
+    public void UpPressed()
+    {
+       
+        upPressed = true;
+    }
+    
+    public void DownNotPress()
+    {
+        
+        downPressed = false;
+
+    }
+    
+    public void DownPressed()
+    {
+        
+        downPressed = true;
+    }
+    
+    public void RightNotPress()
+    {
+        
+        rightPressed = false;
+
+    }
+    
+    public void RightPressed()
+    {
+       
+        rightPressed = true;
+    }
+    
+    public void LeftNotPress()
+    {
+        
+        leftPressed = false;
+
+    }
+    
+    public void LeftPressed()
+    {
+       
+        leftPressed = true;
     }
 
-  
+   
 
   
     
