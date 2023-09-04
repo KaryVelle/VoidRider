625e38008d1727951888e3eb3dfc63e409e92d2c
Carlos R <crono_zs@hotmail.com>
2023-09-04 12:00:21 -0600

    el joystick ya esta ALV

--- VoidRider/Assets/Scripts/Joystic_Char.cs
+++ VoidRider/Assets/Scripts/Joystic_Char.cs
@@ -6,23 +6,26 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Joystic_Char : MonoBehaviour
 {
     public Transform parentObject;
-    public float direccion;
+    public float direccionX;
+    public float direccionY;
     private XRGrabInteractable grabInteractable;
-    [SerializeField] private Vector2 globalPosition;
-
+    [SerializeField] private Vector2 LimitsX;
+    [SerializeField] private Vector2 LimitsY;
+    [SerializeField] private Vector2 desiredLimit;
+    private Vector3 parentSize;
     private Vector3 _inicialPocicion;
     // Start is called before the first frame update
     void Start()
     {
         _inicialPocicion = transform.position;
         grabInteractable = GetComponent<XRGrabInteractable>();
+        parentSize = parentObject.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(transform.localPosition.x);
-        Vector3 parentSize = parentObject.localScale;
         Vector3 relativePosition = parentObject.InverseTransformPoint(transform.position);
 
         float clampedX = Mathf.Clamp(relativePosition.x, -parentSize.x, parentSize.x);
@@ -34,14 +37,23 @@ public class Joystic_Char : MonoBehaviour
 
         if (transform.position.x != 0)
         {
-            direccion = (transform.position.x  - -parentSize.x) / (parentSize.x - parentSize.x) * (globalPosition.x - globalPosition.y) + globalPosition.y;
-            Debug.Log(direccion);
+            direccionX = (transform.localPosition.x - LimitsX.x) / (LimitsX.y - LimitsX.x) * (desiredLimit.x - desiredLimit.y) + desiredLimit.y;
+            direccionY = (transform.localPosition.y - LimitsY.x) / (LimitsY.y - LimitsY.x) * (desiredLimit.x - desiredLimit.y) + desiredLimit.y;
+            Debug.Log(direccionX +", " + direccionY);
+            /*Debug.Log(transform.localPosition.x);
+            Debug.Log(transform.localPosition.y);*/
         }
 
 
         if(grabInteractable.isSelected == false)
             transform.position = _inicialPocicion;
 
+    }
+
+    private void CalculateDirecction()
+    {
+        direccionX = transform.localPosition.x;
+        Debug.Log(direccionX);
     }
 
 }
