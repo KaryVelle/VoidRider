34a03aa27a6be12f720867c0fdc056baceb0de63
Kenneth <alcanamay@gmail.com>
2023-09-05 16:32:29 -0600

    Joints con particulas
    Joints se separan y las particulas se activan dos veces

---
+++ VoidRider/Assets/VOID/Scripts/Explosion.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem explosion;
+    [SerializeField] private GameObject joint;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("boom");
+        Destroy(GetComponent<FixedJoint>());
+        Instantiate(explosion, transform.position, transform.rotation);
+        StartCoroutine(Destruye());
+
+
+    }
+
+    IEnumerator Destruye()
+    {
+        yield return new WaitForSeconds(3);
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(joint);
+        Destroy(gameObject);
+    }
+}
