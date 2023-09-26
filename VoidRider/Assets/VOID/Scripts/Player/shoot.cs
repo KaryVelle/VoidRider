293d73daf680a9586db417e83e7037fec87aa1d6
KaryVelle <kary.velle@gmail.com>
2023-09-26 15:48:19 -0600

    commit

--- VoidRider/Assets/VOID/Scripts/Player/shoot.cs
+++ VoidRider/Assets/VOID/Scripts/Player/shoot.cs
@@ -9,67 +9,41 @@ public class shoot : MonoBehaviour
 {
 
     [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private GameObject bulletPrefab2;
     [SerializeField] private Transform shootFrom1;
     [SerializeField] private Transform shootFrom2;
-    
-    [SerializeField] private Transform shootFromBack1;
-    [SerializeField] private Transform shootFromBack2;
     [SerializeField] private float speed;
-    public float shootMulti = 1f;
-    
     private Rigidbody rb1;
     private Rigidbody rb2;
- 
-    public int destroyBulletTime1 = 10;
-    public int destroyBulletTime2 = 10;
-    public int destroyBulletTime3 = 10;
+    [SerializeField] private bool fire;
 
-  
-
-    public void Shoot1()
+    public void Start()
     {
-        GameObject bullet1= Instantiate(bulletPrefab, shootFrom1.position, Quaternion.identity);
-        rb1 = bullet1.GetComponent<Rigidbody>();
-        rb1.AddForce(transform.forward * speed * shootMulti);
-        Destroy(bullet1, destroyBulletTime3);
-        
-        GameObject bullet2= Instantiate(bulletPrefab, shootFrom2.position, Quaternion.identity);
-        rb2 = bullet2.GetComponent<Rigidbody>();
-        rb2.AddForce(transform.forward * speed * shootMulti);
-        Destroy(bullet2, destroyBulletTime3);
+      
+    }
 
+    public void Update()
+    {
+        if (fire)
+        {
+            Shoot();
+        }
+    }
+
+
+    public void Shoot()
+    {
+        GameObject bullet1= Instantiate(bulletPrefab, shootFrom1.position, Quaternion.Euler(0,0,0));
+        rb1 = bullet1.GetComponent<Rigidbody>();
+        rb1.AddForce(transform.forward * speed);
+        Destroy(bullet1, 3);
+        
+        GameObject bullet2= Instantiate(bulletPrefab, shootFrom2.position, Quaternion.Euler(0,0,0));
+        rb2 = bullet2.GetComponent<Rigidbody>();
+        rb2.AddForce(transform.forward * speed);
+        Destroy(bullet2, 10);
         
 
     }
-    
-    public void Shoot2()
-    {
-        GameObject bullet1= Instantiate(bulletPrefab2, shootFrom1.position, Quaternion.identity);
-        rb1 = bullet1.GetComponent<Rigidbody>();
-        rb1.AddForce(transform.forward * speed * shootMulti);
-        Destroy(bullet1, destroyBulletTime2);
-        
-        GameObject bullet2= Instantiate(bulletPrefab2, shootFrom2.position, Quaternion.identity);
-        rb2 = bullet2.GetComponent<Rigidbody>();
-        rb2.AddForce(transform.forward * speed * shootMulti);
-        Destroy(bullet2, destroyBulletTime2);
-        
 
-    }
-    
-    public void Shoot3()
-    {
-        GameObject bullet1= Instantiate(bulletPrefab, shootFromBack1.position, Quaternion.identity);
-        rb1 = bullet1.GetComponent<Rigidbody>();
-        rb1.AddForce(transform.forward * speed * -1 * shootMulti);
-        Destroy(bullet1, destroyBulletTime3);
-        
-        GameObject bullet2= Instantiate(bulletPrefab, shootFromBack2.position, Quaternion.identity);
-        rb2 = bullet2.GetComponent<Rigidbody>();
-        rb2.AddForce(transform.forward * speed * -1 * shootMulti);
-        Destroy(bullet2, destroyBulletTime3);
-        
-    }
- 
+   
 }
