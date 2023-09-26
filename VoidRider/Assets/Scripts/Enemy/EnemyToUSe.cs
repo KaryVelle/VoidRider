5828119c955858b860a743d397713dbd60ac530f
KaryVelle <kary.velle@gmail.com>
2023-09-26 17:33:20 -0600

    Enemy scripts

--- VoidRider/Assets/Scripts/Enemy/EnemyToUSe.cs
+++ VoidRider/Assets/Scripts/Enemy/EnemyToUSe.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class EnemyToUSe : Enemy
 {
+    [SerializeField] private FixedJoint joint;
+    [SerializeField] private Rigidbody rbj1;
+    [SerializeField] private Rigidbody rbj2;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private bool isDestroy = false;
+    private BoxCollider _bc;
+    private Coroutine shotDelay;
+
     private void Start()
     {
+        Fire = false;
         Bala = BalaPrefab.GetComponent<BulletToUse>();
+        _bc = GetComponent<BoxCollider>();
     }
 
     private void Update()
@@ -15,29 +26,54 @@ public class EnemyToUSe : Enemy
         {
             Shot();
         }
+        if(isDestroy)
+        {
+            rbj1.transform.Rotate(Vector3.one * rotSpeed * Time.deltaTime);
+            rbj2.transform.Rotate(Vector3.one * rotSpeed * Time.deltaTime * -1);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "Bullet")
+        if (other.transform.tag == "PlayerBullet")
         {
             Debug.LogWarning("quepedo");
             HP -= Bala.Damage;
-            if (HP <= 0) Destroy(gameObject);
-            //DestroyAnim.Play();
-            //Destroy(gameObject);
+            if (HP <= 0)
+            {
+                Destroy(joint);
+                _bc.enabled = false;
+                isDestroy = true;
+                rbj2.AddForce(Vector3.one * speed, ForceMode.Impulse);
+                rbj1.AddForce((Vector3.one*-1 * speed), ForceMode.Impulse);
+                Instantiate(DestroyAnim, transform.position, transform.rotation);
+                StartCoroutine(Destruye());
+            }
+
         }
     }
 
     private void Shot()
     {
         Fire = false;
-        Instantiate(BalaPrefab, shootPos1.position, Quaternion.Euler(0, 0, 0));
-        StartCoroutine(DelayDeBala());
+        //BalaPrefab.transform.position = shootPos1.position;
+        if(shotDelay == null)
+        {
+            Instantiate(BalaPrefab, shootPos1.position, shootPos1.rotation);
+            shotDelay = StartCoroutine(DelayDeBala());
+        }
     }
 
     IEnumerator DelayDeBala()
     {
         yield return new WaitForSeconds(Delay);
-        Fire = true;
+        shotDelay = null;
+    }
+
+    IEnumerator Destruye()
+    {
+        yield return new WaitForSeconds(3);
+        Instantiate(DestroyAnim, transform.position, transform.rotation);
+        Destroy(joint);
+        Destroy(gameObject);
     }
 }
--- VoidRider/Assets/Scripts/IAEnemies/EnemyController.cs
+++ VoidRider/Assets/Scripts/IAEnemies/EnemyController.cs
@@ -4,17 +4,73 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private BehavorController behavorController;
+    [SerializeField] private Wander wander;
+    [SerializeField] private Pursuit pursuit;
+    [SerializeField] private EnemyToUSe enemyToUSe;
     public Steering stering;
     public GameObject target;
+    public enum EnemyState
+    {
+        Idle,
+        Pursuit,
+        Evade,
+        Attack
+    }
+    public EnemyState myState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        behavorController = GetComponent<BehavorController>();
+        pursuit = GetComponent<Pursuit>();
+        wander = GetComponent<Wander>();
+        enemyToUSe = GetComponent<EnemyToUSe>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (myState)
+        {
+            case EnemyState.Idle:
+                behavorController.behavors[0] = wander;
+                pursuit.enabled = false;
+                wander.enabled = true;
+                enemyToUSe.Fire = false;
+                break;
+            case EnemyState.Pursuit:
+                behavorController.behavors[0] = pursuit;
+                pursuit.evade = false;
+                pursuit.enabled = true;
+                wander.enabled = false;
+                enemyToUSe.Fire = false;
+                break;
+            case EnemyState.Evade:
+                behavorController.behavors[0] = pursuit;
+                pursuit.evade = true;
+                pursuit.enabled = true;
+                wander.enabled = false;
+                enemyToUSe.Fire = false;
+                break;
+            case EnemyState.Attack:
+                behavorController.behavors[0] = pursuit;
+                pursuit.evade = false;
+                pursuit.enabled = true;
+                wander.enabled = false;
+                enemyToUSe.Fire = true;
+                if ((target.transform.position - transform.position).magnitude < 30f)
+                    myState = EnemyState.Evade;
+                break;
+
+        }
         stering.Target = target.transform.position;
+        //myState = EnemyState.Evade;
+    }
+
+   
+    void EvaluateState()
+    {
+        //if(hp<1)
+        //{ myState = EnemyState.Evade; }
     }
 }
